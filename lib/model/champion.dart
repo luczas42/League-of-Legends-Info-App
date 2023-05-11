de8d32a26296707b86421ffb798f9d9250817ebf
Lucas Santos <110632756+luczas42@users.noreply.github.com>
2023-05-11 10:12:25 -0300

    feat: criado método para popular objeto a partir de json

--- lib/model/champion.dart
+++ lib/model/champion.dart
@@ -13,13 +13,56 @@ class ChampionResponse {
 
 class Champion {
   String skinUrl = "";
+  String title = "";
   String splashUrl = "";
   final String championName;
   String skinName = "";
+  String blurb = "";
 
-  Champion({required this.championName});
+  Champion(
+      {required this.championName,
+      required this.splashUrl,
+      required this.title,
+      required this.blurb});
 
   factory Champion.fromJson(Map<String, dynamic> json) {
-    return Champion(championName: json['name']);
+    return Champion(
+      championName: json['name'],
+      splashUrl: json['id'],
+      title: json['title'],
+      blurb: json['blurb'],
+    );
+  }
+
+  String titleToCapitalized() {
+    return title[0].toUpperCase()+title.substring(1);
   }
 }
+
+// List<Champion> championList = [
+//   Champion(
+//     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Samira_20.jpg',
+//     'Samira',
+//     'High Noon Samira',
+//   ),
+//   Champion(
+//     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_13.jpg',
+//     'Jinx',
+//     'Odyssey Jinx',
+//   ),
+//   Champion(
+//     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_7.jpg',
+//     'Aatrox',
+//     'Blood Moon Aatrox',
+//   ),
+//   Champion(
+//     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_5.jpg',
+//     'Jhin',
+//     'Dark Cosmic Jhin',
+//   ),
+//   Champion(
+//     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Gwen_11.jpg',
+//     'Gwen',
+//     'Cafe Cuties Gwen',
+//   ),
+// ];
